3db30123583cf1eb66ebac39f45850dd6c5912d5
SeanB <amici_tarsus.0m@icloud.com>
2025-02-26 11:43:16 -0800

    Header and nav for desktop, tablet and mobile

---
+++ TestOut.js
@@ -0,0 +1,37 @@
+/* const creature = "Golem";
+
+console.log("The " + creature + " under the bed!");
+ */
+
+/* const a = 5;
+const b = 8;
+console.log(`Sum = ${a+b})`;
+ */
+
+/* const apartment = {
+  numRooms: 1,
+  includeWasherDryer: true,
+  petsAllowed: false
+}
+
+apartment.petsAllowed = true;
+console.log(apartment);
+ */
+
+/* const creature = 'Golem';
+
+console.log(`A ${creature} appeared!`); */
+
+/* const numberOne = 40;
+const numberTwo = 6;
+const operation = 'sum';
+ */
+
+const letters = 'ABCxyz';
+
+for (let i = 0; i < 3; i += 1) {
+  for (let j = 3; j < 6; j += 1) {
+    console.log(`${letters[i]}${letters[j]}`);
+  }
+}
+
